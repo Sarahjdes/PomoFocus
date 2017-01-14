15304a7ee0c35c75b24e0d38229bf31bae1343f1
Juan Carlos Alvarado Valdivia <vacj22@users.noreply.github.com>
2017-01-14 03:23:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,114 +1,174 @@
-window.onload = pageLoaded();
+var clock = {
+	"minutes" : 25,
+	"seconds" : 00,
+	"initMinutes" : 25,
+	"initSeconds" : 00,
+	"pomodoroFlag" : 0
+};
 
-var minutes = 25;
-var seconds = 00;
-var initMinutes = 25;
-var initSeconds = 00;
-var pomodoroFlag = 0;
+window.onload = pageLoaded();
 
 function pageLoaded(){
 	var startBtn = document.getElementById("startBtn");
 	var stopBtn = document.getElementById("stopBtn");
 	var resetBtn = document.getElementById("resetBtn");
+	var settingsBox = document.getElementById("settingsBox");
 	var settingsBtn = document.getElementById("settingsBtn");
 	var backBtn = document.getElementById("backBtn");
 	var cancelBtn = document.getElementById("cancelAddBtn");
 	var addBtn = document.getElementById("addBtn");
+	var mainBox = document.getElementById("container");
+	var customBox = document.getElementById("customizeTimeBox");
+	var customBtn = document.getElementById("customBtn");
+	var backCustom = document.getElementById("backCustom");
+	var addNewTaskBox = document.getElementById("addNewTaskBox");
+	var addNewTaskBtn = document.getElementById("addNewTaskBtn");
+	var app = {
+		"settings" : {
+			/* App settings */
+			"userTheme" : "Green",
+			"alarmSong" : "Happy Song",
+			"alarmVolume" : "70",
+			"alarmVibrate" : "false"
+		}, 
+		"stats" : {
+			/* some basic pomodoro stats and an array of pomodoro objects */
+			/* each pomodoro objects contains: date, duration and state, there are 3 possible states
+			 	- success: the pomodoro was finished successfully
+			 	- failure: the user failed to finish the pomodoro
+			 	- none: the pomodoro has not been executed yet */
+			"finishedPomos" : "0",
+			"success": "0",
+			"failed": "0",
+			"pomodoros" : [
+				{
+					"pomodoro" : {
+						"date" : "27/03/2017",
+						"duration" : "25", // duration is in minutes
+						"state": "none"
+					}
+				},
+				{
+					"pomodoro" : {
+						"date" : "18/07/2017",
+						"duration" : "10",
+						"state": "none"
+					}
+				}
+			]
+		}
+	};
+	
+	loadSettings(app);
 	
 	startBtn.onclick = startPomodoro;
 	stopBtn.onclick = stopPomodoro;
 	resetBtn.onclick = resetPomodoro;
-	settingsBtn.onclick = settingsPanel;
-	backBtn.onclick = updateTime;
-	cancelBtn.onclick = goBack;
-	addBtn.onclick = addTask;
+	addBtn.onclick = function addTask(){
+		// Shows the add new task menu and waits for user input
+		addNewTaskBox.style.display = 'block';
+		mainBox.style.display = 'none';
+		addNewTaskBtn.onclick = function goBack(){
+			// Hides the add menu and shows the main screen
+			addNewTaskBox.style.display = 'none';
+			mainBox.style.display = 'block';
+			// add the new task to the app object, then save the app state
+		};
+		cancelBtn.onclick = function goBack(){
+			addNewTaskBox.style.display = 'none';
+			mainBox.style.display = 'block';
+		};
+	};
+	settingsBtn.onclick = function settingsPanel(){
+		// Shows the settings panel and waits for user input
+		settingsBox.style.display = 'block';
+		mainBox.style.display = 'none';
+		backBtn.onclick = updateUserSettings;
+		/* Update time is for custom function */
+	};
+	customBtn.onclick = function showCustom(){
+		customBox.style.display = 'block';
+		backCustom.onclick = updateTime;
+	};
 	
-	if(typeof(Storage) != "undefined"){
-	}else{
-		// No web Storage support
-	}
 }
-
 function startPomodoro(){
 	// This function will start a new pomodoro once the start or reset buttons have been clicked.
-	if(pomodoroFlag  == 0){
+	if(clock.pomodoroFlag  == 0){
 		// If one second has passed then decrease time
-		pomodoroFlag = setInterval(decreaseTime, 1000);
+		clock.pomodoroFlag = setInterval(decreaseTime, 1000);
 	}
 }
 function stopPomodoro(){
 	// Stop the pomodoro clock from decreasing time
-	clearInterval(pomodoroFlag);
-	pomodoroFlag = 0;
+	clearInterval(clock.pomodoroFlag);
+	clock.pomodoroFlag = 0;
 }
 function resetPomodoro(){
 	var timeShown = document.getElementById("time");
 	stopPomodoro();
 	// Restart the shown time to the initial time
-	minutes = initMinutes;
-	seconds = initSeconds;
-	timeShown.innerHTML = minutes + " : 00";
+	clock.minutes = clock.initMinutes;
+	clock.seconds = clock.initSeconds;
+	timeShown.innerHTML =clock.minutes + " : 00";
 	startPomodoro();
 }
 function decreaseTime(){
 	// This function is called every second after a pomodoro is started
 	var timeShown = document.getElementById("time");
-	if(minutes == 0 && seconds == 0){
+	if(clock.minutes == 0 && clock.seconds == 0){
 		// Pomodoro Completed
 		stopPomodoro();	 
-	}else if(seconds == 0){
-		minutes--;
-		seconds = 59;
+	}else if(clock.seconds == 0){
+		clock.minutes--;
+		clock.seconds = 59;
 	}else{
-		seconds--;
+		clock.seconds--;
 	}
 	// Show current pomodoro time
-	if(seconds < 10){
-		timeShown.innerHTML = minutes + " : 0" + seconds;
+	if(clock.seconds < 10){
+		timeShown.innerHTML = clock.minutes + " : 0" + clock.seconds;
 	}else{
-		timeShown.innerHTML = minutes + " : " + seconds;	
+		timeShown.innerHTML = clock.minutes + " : " + clock.seconds;	
 	}
 }
-
-/* Settings functions */
-function settingsPanel(){
-	// Shows the settings panel
-	var settingsBox = document.getElementById("settingsBox");
-	var containerBox = document.getElementById("container");
-	settingsBox.style.display = 'block';
-	containerBox.style.display = 'none';
-	//containerBox.style.position = 'absolute';
-}
 function updateTime(){
-	// Executes when the user closes the settings panel
 	// Sets the time and initial time variables as the number entered by the user
+	var customBox = document.getElementById("customizeTimeBox");
 	var timeShown = document.getElementById("time");
 	var userMinutes = document.getElementById("userMinutes");
 	var userSeconds = document.getElementById("userSeconds");
-	var settingsBox = document.getElementById("settingsBox");
-	var mainBox = document.getElementById("container");
 	
-	initMinutes = userMinutes.value;
-	initSeconds = userSeconds.value;
-	minutes = userMinutes.value;
-	seconds = userSeconds.value;
-	timeShown.innerHTML = minutes + " : " + seconds;
-	settingsBox.style.display = 'none';
-	mainBox.style.display = 'block';
+	clock.initMinutes = userMinutes.value;
+	clock.initSeconds = userSeconds.value;
+	clock.minutes = userMinutes.value;
+	clock.seconds = userSeconds.value;
+	timeShown.innerHTML = clock.minutes + " : " + clock.seconds;
+	customBox.style.display = 'none';
 }
-
-/* Add new Task functions */
-function addTask(){
-	// Shows the add new task menu and waits for user input
-	var mainBox = document.getElementById("container");
-	var addNewTaskBox = document.getElementById("addNewTaskBox");
-	addNewTaskBox.style.display = 'block';
-	mainBox.style.display = 'none';
+function updateUserSettings(){
+	// updates the app object settings with the user entered settings	
 }
-function goBack(){
-	// Hides the add menu and shows the main screen
-	var addNewTask = document.getElementById("addNewTaskBox");
-	var mainBox = document.getElementById("container");
-	addNewTask.style.display = 'none';
-	mainBox.style.display = 'block';
+function loadSettings(app){
+	if(typeof(Storage) != "undefined"){
+		if(localStorage.getItem("app") != null){
+			// Si es que hay opciones guardadas
+			app = JSON.parse(localStorage.getItem("app"));
+		}else{
+			// Es la primera vez que se ejecuta la aplicacion
+			localStorage.setItem("app", JSON.stringify(app));
+		}
+	}else{
+		// No web Storage support
+	}
+}
+function saveAppState(app){
+	if(typeof(Storage) != "undefined"){
+		localStorage.setItem("app", JSON.stringify(app));
+	}
+}
+function loadAppState(app){
+	if(typeof(Storage) != "undefined"){
+		app = JSON.parse(localStorage.getItem("app"));
+	}
 }

c1ac832bb5c31cc6834045332023834d57e9ec8f
Juan Carlos Alvarado Valdivia <vacj22@users.noreply.github.com>
2017-02-03 19:14:36 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ function pageLoaded(){
 	var history = document.getElementById("history");
 	var historyBtn = document.getElementById("historyBtn");
 	var tasks = document.getElementById("tasks");
-
+	var fieldsAlert = document.getElementById("fieldsAlert");
+	var fieldsOk = document.getElementById("fieldsOk");
 	// Load user settings from localStorage and update the app object
 	loadAppState();
 	// Put the values in the actual dom
@@ -71,13 +72,23 @@ function pageLoaded(){
 		mainBox.classList.add("hideMainLeft");
 		addNewTaskBtn.onclick = function saveTask(){
 			// add the new task to the app object
-			addTask();
-			// Save the app state to localStorage
-			saveAppState();
-			// Re-load the task list
-			loadAppDom();
-			addNewTaskBox.classList.remove("showNewTask");
-			mainBox.classList.remove("hideMainLeft");
+			if(filledForm() === true){
+				addTask();
+				saveAppState();
+				loadAppDom();			// Re-load the task list
+				document.getElementById("taskName").value = "";
+				addNewTaskBox.classList.remove("showNewTask");
+				mainBox.classList.remove("hideMainLeft");
+			}else{
+				shadowBox.style.pointerEvents = 'auto';
+				shadowBox.classList.add("showShadowBox");
+				fieldsAlert.classList.add("showAlert");
+				fieldsOk.onclick = function(){
+					shadowBox.style.pointerEvents = 'none';
+					shadowBox.classList.remove("showShadowBox");
+					fieldsAlert.classList.remove("showAlert");
+				}
+			}
 		};
 		cancelBtn.onclick = function goBack(){
 			addNewTaskBox.classList.remove("showNewTask");
@@ -102,10 +113,15 @@ function pageLoaded(){
 		  settingsBox.classList.remove("showSettings");
 		};
 	};
-	/*customBtn.onclick = function showCustom(){
-		customBox.style.display = 'block';
-		backCustom.onclick = updateTime;
-	};*/
+}
+
+function filledForm(){
+	// check if the add form is filled
+	if(document.getElementById("taskName").value.length > 0 && document.getElementById("taskStartTime").value != ""){
+		return true;
+	}else {
+		return false;
+	}
 }
 
 function activeTask(task, time){
@@ -122,7 +138,8 @@ function startPomodoro(){
 	// This function will start a new pomodoro once the start or reset buttons have been clicked.
 	var currentTask = app.currentTask;
 	if(activeTask(currentTask) === true){
-		document.getElementById("minorText").innerHTML = currentTask.name;
+		// set the minutes and seconds
+		document.getElementById("minorText").innerHTML = "Working on: " + currentTask.name;
 		if(clock.pomodoroFlag  == 0){
 			// If one second has passed then decrease time
 			clock.pomodoroFlag = setInterval(decreaseTime, 1000);
@@ -360,32 +377,6 @@ function fixTime(year, month, day){
 	return fixedDate;
 }
 
-function addTask(){
-	var newTask = {};
-	newTask.id = randomId();
-	newTask.name = document.getElementById("taskName").value;
-	newTask.date = document.getElementById("taskDate").value;
-	newTask.startTime = document.getElementById("taskStartTime").value;
-	newTask.duration = document.getElementById("duration").value;
-	newTask.break = document.getElementById("breakTime").value;
-	newTask.longBreak = document.getElementById("longBreakTime").value;
-	newTask.startNow = false;
-	// SOLVE TAGS SYSTEM
-	app.ids.push(newTask.id);
-	app.pomodoros.push(newTask);
-}
-
-function randomId(){
-	// create and return a valid id
-	var validNumber = false;
-	var number = 0;
-	while(validNumber !== true){
-		number = Math.floor((Math.random() * 100000000000) + 1); // Creates a random number between 1 and 100000000000
-		validNumber = alreadyExists(number);
-	}
-	return number.toString();
-}
-
 function alreadyExists(num){
 	// Implement merge sort later
 	var ids = app.ids;
@@ -456,6 +447,32 @@ function loadAppDom(){
 	}
 }
 
+function addTask(){
+	var newTask = {};
+	newTask.id = randomId();
+	newTask.name = document.getElementById("taskName").value;
+	newTask.date = document.getElementById("taskDate").value;
+	newTask.startTime = document.getElementById("taskStartTime").value;
+	newTask.duration = document.getElementById("duration").value;
+	newTask.break = document.getElementById("breakTime").value;
+	newTask.longBreak = document.getElementById("longBreakTime").value;
+	newTask.startNow = false;
+	// SOLVE TAGS SYSTEM
+	app.ids.push(newTask.id);
+	app.pomodoros.push(newTask);
+}
+
+function randomId(){
+	// create and return a valid id
+	var validNumber = false;
+	var number = 0;
+	while(validNumber !== true){
+		number = Math.floor((Math.random() * 100000000000) + 1); // Creates a random number between 1 and 100000000000
+		validNumber = alreadyExists(number);
+	}
+	return number.toString();
+}
+
 function addDomTask(task){
 	var tasks = document.getElementById("tasks");
 	var container = document.createElement("div");
@@ -464,19 +481,38 @@ function addDomTask(task){
 	var icon = document.createElement("i");
 	var editIcon = document.createElement("i");
 	var jump = document.createElement("br");
+	var shadowBox = document.getElementById("shadowBox");
+	var startNowBtnY = document.getElementById("startYes");
+	var startNowBtnN = document.getElementById("startNo");
 
 	container.classList.add("task");
 	title.classList.add("taskText");
 	subTitle.classList.add("subTask");
 	icon.classList.add("fa", "fa-circle-o", "fa-lg", "taskState");
-	editIcon.classList.add("fa", "fa-ellipsis-v", "fa-lg", "taskTime");
+	editIcon.classList.add("fa", "fa-ellipsis-v", "fa-lg", "taskTime", "editIcon");
 
+	title.appendChild(icon);
 	title.innerHTML += " " + task.name;
 	subTitle.innerHTML = task.startTime + " - " + task.duration + " minutes";
 
-	title.appendChild(icon);
 	title.onclick = function (){
 		// abrir mensaje preguntando si quieres empezar el pomodoro ahora
+		shadowBox.style.pointerEvents = 'auto';
+		shadowBox.classList.add("showShadowBox");
+		startNow.classList.add("showAlert");
+		startNowBtnY.onclick = function(){
+			task.startNow = true;
+			app.currentTask = task;
+			shadowBox.style.pointerEvents = 'none';
+			shadowBox.classList.remove("showShadowBox");
+			startNow.classList.remove("showAlert");
+			startPomodoro();
+		};
+		startNowBtnN.onclick = function(){
+			shadowBox.style.pointerEvents = 'none';
+			shadowBox.classList.remove("showShadowBox");
+			startNow.classList.remove("showAlert");
+		};
 	};
 	editIcon.onclick = function (){
 		// editar la tarea
@@ -491,20 +527,80 @@ function addDomTask(task){
 }
 
 function editTask(taskId){
-	// encontrar la task a la que le pertenece este id
-	var pomodoros = app.pomodoros;
+	// Show a popup window to edit the selected task
 	var task;
+	var pomodoros = app.pomodoros;
+	var position, idIndex;
 	var window = document.getElementById("editWindow");
+	var shadowBox = document.getElementById("shadowBox");
+	var taskName = document.getElementById("editTaskName");
+	var taskDate = document.getElementById("editTaskDate");
+	var startTime = document.getElementById("editTaskStartTime");
+	var taskDuration = document.getElementById("editDuration");
+	var taskBreak = document.getElementById("editBreakTime");
+	var taskLongBreak = document.getElementById("editLongBreakTime");
+	var saveBtn = document.getElementById("addChangesBtn");
+	var cancelBtn = document.getElementById("cancelEditBtn");
+	var deleteTask = document.getElementById("deleteTaskBtn");
+	var deletePrompt = document.getElementById("deletePrompt");
+	var deleteYes = document.getElementById("deleteYes");
+	var deleteNo = document.getElementById("deleteNo");
+	// We search the task for the given id (the task id will exist until the task is deleted)
 	for(var i = 0; i < pomodoros.length; i++){
 		if(pomodoros[i].id === taskId){
 			task = pomodoros[i];
+			position = i;
 			break;
 		}
 	}
-	window.classList.add("showEditW");
-	document.getElementById("editTaskName").value = task.name;
-	document.getElementById("editTaskName").value = task.startTime;
-	document.getElementById("editTaskName").value = task.tags;
-	document.getElementById("editTaskName").value = task.name;
+	// We set the default popup values
+	taskName.value = task.name;
+	taskDate.value = task.date;
+	startTime.value = task.startTime;
+	taskDuration.value = task.duration;
+	taskBreak.value = task.break;
+	taskLongBreak.value = task.longBreak;
+	// We show the popup window
+	shadowBox.style.pointerEvents = 'auto';
+	shadowBox.classList.add("showShadowBox");
+	window.classList.add("showAlert");
 
+	saveBtn.onclick = function (){
+		app.pomodoros[position].name = taskName.value;
+		app.pomodoros[position].date = taskDate.value;
+		app.pomodoros[position].startTime = startTime.value;
+		app.pomodoros[position].duration = taskDuration.value;
+		app.pomodoros[position].break = taskBreak.value;
+		app.pomodoros[position].longBreak = taskLongBreak.value;
+		saveAppState();
+		loadAppDom();
+		shadowBox.style.pointerEvents = 'none';
+		shadowBox.classList.remove("showShadowBox");
+		window.classList.remove("showAlert");
+	};
+	cancelBtn.onclick = function (){
+		shadowBox.style.pointerEvents = 'none';
+		shadowBox.classList.remove("showShadowBox");
+		window.classList.remove("showAlert");
+	};
+	deleteTask.onclick = function (){
+		window.classList.remove("showAlert");
+		deletePrompt.classList.add("showAlert");
+		deleteYes.onclick = function(){
+			// delete task
+			app.pomodoros.splice(position, 1);
+			// delete task id
+			idIndex = app.ids.indexOf(taskId);
+			app.ids.splice(idIndex, 1);
+			saveAppState();
+			loadAppDom();
+			shadowBox.style.pointerEvents = 'none';
+			shadowBox.classList.remove("showShadowBox");
+			deletePrompt.classList.remove("showAlert");
+		};
+		deleteNo.onclick = function(){
+			deletePrompt.classList.remove("showAlert");
+			window.classList.add("showAlert");
+		};
+	};
 }

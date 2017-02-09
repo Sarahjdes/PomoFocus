f321b3cb47a2c39d1d5c1ee0f073f63e19664f25
Juan Carlos Alvarado Valdivia <vacj22@users.noreply.github.com>
2017-02-08 23:45:45 -0300

    Create en.js

---
+++ localization/en.js
@@ -0,0 +1,90 @@
+var language = [
+  {
+    "textId" : "changeStyle",
+    "text" : "Change Style"
+  },
+  {
+    "textId" : "alarmSound",
+    "text" : "Alarm Sound"
+  },
+  {
+    "textId" : "alarmVolume",
+    "text" : "Alarm Volume"
+  },
+  {
+    "textId" : "vibrateText",
+    "text" : "Vibrate when the alarm sounds"
+  },
+  {
+    "textId" : "sendFeedBack",
+    "text" : "Send FeedBack"
+  },
+  {
+    "textId" : "rateApp",
+    "text" : "Rate this app"
+  },
+  {
+    "textId" : "addTaskTitle",
+    "text" : "Add New Task"
+  },
+  {
+    "textId" : "taskNameText",
+    "text" : "Task Name"
+  },
+  {
+    "textId" : "studyTag",
+    "text" : "Study"
+  },
+  {
+    "textId" : "workTag",
+    "text" : "Work"
+  },
+  {
+    "textId" : "researchTag",
+    "text" : "Research"
+  },
+  {
+    "textId" : "sportTag",
+    "text" : "Sport"
+  },
+  {
+    "textId" : "startTimeText",
+    "text" : "Start Time"
+  },
+  {
+    "textId" : "lengthText",
+    "text" : "Pomodoro Length"
+  },
+  {
+    "textId" : "shortBreakText",
+    "text" : "Short Break Length"
+  },
+  {
+    "textId" : "longBreakText",
+    "text" : "Long Break Length"
+  },
+  {
+    "textId" : "addNewTaskBtn",
+    "text" : "Add"
+  },
+  {
+    "textId" : "statsTitle",
+    "text" : "Showing stats for"
+  },
+  {
+    "textId" : "minorText",
+    "text" : "No active tasks"
+  },
+  {
+    "textId" : "color1",
+    "text" : "Green"
+  },
+  {
+    "textId" : "color2",
+    "text" : "Blue"
+  },
+  {
+    "textId" : "color3",
+    "text" : "Pink"
+  },
+];

be7baf66e43d4d8ca10780158a641a4b0a8a2bff
Juan Carlos Alvarado Valdivia <vacj22@users.noreply.github.com>
2017-02-09 01:05:14 -0300

    Create es.js

---
+++ localization/es.js
@@ -0,0 +1,266 @@
+var language = [
+  {
+    "textId" : "tasksBtn",
+    "text" : "Tareas"
+  },
+  {
+    "textId" : "statsBtn",
+    "text" : "Estadisticas"
+  },
+  {
+    "textId" : "historyBtn",
+    "text" : "Historial"
+  },
+  {
+    "textId" : "settingsTitleText",
+    "text" : "Ajustes"
+  },
+  {
+    "textId" : "changeStyle",
+    "text" : "Cambiar Estilo"
+  },
+  {
+    "textId" : "alarmSound",
+    "text" : "Alarm Sound"
+  },
+  {
+    "textId" : "alarmVolume",
+    "text" : "Sonido de Alarma"
+  },
+  {
+    "textId" : "noAlarm",
+    "text" : "Sin Alarma"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Cancelar"
+  },
+  {
+    "textId" : "saveAlarmBox",
+    "text" : "Guardar"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Cancelar"
+  },
+  {
+    "textId" : "vibrateText",
+    "text" : "Vibrar cuando suene la alarma"
+  },
+  {
+    "textId" : "sendFeedBack",
+    "text" : "Enviar FeedBack"
+  },
+  {
+    "textId" : "rateApp",
+    "text" : "Calificar esta aplicación"
+  },
+  {
+    "textId" : "addTaskTitle",
+    "text" : "Añadir nueva tarea"
+  },
+  {
+    "textId" : "taskNameText",
+    "text" : "Nombre"
+  },
+  {
+    "textId" : "studyTag",
+    "text" : "Estudiar"
+  },
+  {
+    "textId" : "workTag",
+    "text" : "Trabajar"
+  },
+  {
+    "textId" : "researchTag",
+    "text" : "Investigar"
+  },
+  {
+    "textId" : "sportTag",
+    "text" : "Deporte"
+  },
+  {
+    "textId" : "startTimeText",
+    "text" : "Hora de inicio"
+  },
+  {
+    "textId" : "lengthText",
+    "text" : "Largo del Pomodoro"
+  },
+  {
+    "textId" : "shortBreakText",
+    "text" : "Largo del descanso corto"
+  },
+  {
+    "textId" : "longBreakText",
+    "text" : "Largo del descanso largo"
+  },
+  {
+    "textId" : "addNewTaskBtn",
+    "text" : "Añadir"
+  },
+  {
+    "textId" : "statsTitle",
+    "text" : "Mostrando Estadisticas para"
+  },
+  {
+    "textId" : "pomodoroTotalTime",
+    "text" : "Tiempo total de Pomodoros:"
+  },
+  {
+    "textId" : "pomodorosFinished",
+    "text" : "Pomodoros Terminados:"
+  },
+  {
+    "textId" : "successRate",
+    "text" : "Porcentaje de Éxito:"
+  },
+  {
+    "textId" : "seeChartBtn",
+    "text" : "Ver Gráficos"
+  },
+  {
+    "textId" : "minorText",
+    "text" : "No hay tareas activas"
+  },
+  {
+    "textId" : "selectedTheme",
+    "text" : "Verde"
+  },
+  {
+    "textId" : "greenColor",
+    "text" : "Verde"
+  },
+  {
+    "textId" : "blueColor",
+    "text" : "Azul"
+  },
+  {
+    "textId" : "pinkColor",
+    "text" : "Rosa"
+  },
+  {
+    "textId" : "classicColor",
+    "text" : "Clasico"
+  },
+  {
+    "textId" : "brightColor",
+    "text" : "Brillante"
+  },
+  {
+    "textId" : "cancelThemeBox",
+    "text" : "Cancelar"
+  },
+  {
+    "textId" : "saveThemeBox",
+    "text" : "Guardar"
+  },
+  {
+    "textId" : "confirmDeleteText",
+    "text" : "Estas seguro de querer eliminar esta tarea?"
+  },
+  {
+    "textId" : "deleteNo",
+    "text" : "No"
+  },
+  {
+    "textId" : "deleteYes",
+    "text" : "Si"
+  },
+  {
+    "textId" : "errorText",
+    "text" : "Error"
+  },
+  {
+    "textId" : "errorMsg",
+    "text" : "Porfavor llena todos los campos antes de agregar tareas"
+  },
+  {
+    "textId" : "congratulationsText",
+    "text" : "Felicitaciones!"
+  },
+  {
+    "textId" : "congratsText",
+    "text" : "Genial! Terminaste un pomodoro!"
+  },
+  {
+    "textId" : "startBreakText",
+    "text" : "Iniciar descanso corto"
+  },
+  {
+    "textId" : "startLongBreakText",
+    "text" : "Iniciar descanso largo"
+  },
+  {
+    "textId" : "quitText",
+    "text" : "Salir"
+  },
+  {
+    "textId" : "statsTitleText",
+    "text" : "Estadisticas"
+  },
+  {
+    "textId" : "donutChartBtn",
+    "text" : "Grafico en Dona"
+  },
+  {
+    "textId" : "barChartBtn",
+    "text" : "Grafico en Barra"
+  },
+  {
+    "textId" : "chartBackBtn",
+    "text" : "Volver"
+  },
+  {
+    "textId" : "editTaskText",
+    "text" : "Editar Tarea"
+  },
+  {
+    "textId" : "editTaskNameText",
+    "text" : "Nombre de la Tarea"
+  },
+  {
+    "textId" : "editStartTimeText",
+    "text" : "Hora de Inicio"
+  },
+  {
+    "textId" : "editPLength",
+    "text" : "Largo del Pomodoro"
+  },
+  {
+    "textId" : "editSbreakText",
+    "text" : "Largo del descanso corto"
+  },
+  {
+    "textId" : "editLBreakText",
+    "text" : "Largo del descanso largo"
+  },
+  {
+    "textId" : "deleteTaskText",
+    "text" : "Borrar Tarea"
+  },
+  {
+    "textId" : "cancelEditBtn",
+    "text" : "Cancelar"
+  },
+  {
+    "textId" : "addChangesBtn",
+    "text" : "Guardar"
+  },
+  {
+    "textId" : "startTaskText",
+    "text" : "Iniciar Tarea"
+  },
+  {
+    "textId" : "startWorkText",
+    "text" : "Quieres comenzar a trabajar en esta tarea ahora?"
+  },
+  {
+    "textId" : "startNo",
+    "text" : "No"
+  },
+  {
+    "textId" : "startYes",
+    "text" : "Si"
+  }
+];

a7a55a11a887abc95aee94ed38ff75d1c5cc6c47
Juan Carlos Alvarado Valdivia <vacj22@users.noreply.github.com>
2017-02-09 00:57:19 -0300

    Update en.js

--- localization/en.js
+++ localization/en.js
@@ -1,5 +1,21 @@
 var language = [
   {
+    "textId" : "tasksBtn",
+    "text" : "Tasks"
+  },
+  {
+    "textId" : "statsBtn",
+    "text" : "Statistics"
+  },
+  {
+    "textId" : "historyBtn",
+    "text" : "History"
+  },
+  {
+    "textId" : "settingsTitleText",
+    "text" : "Settings"
+  },
+  {
     "textId" : "changeStyle",
     "text" : "Change Style"
   },
@@ -10,6 +26,22 @@ var language = [
   {
     "textId" : "alarmVolume",
     "text" : "Alarm Volume"
+  },
+  {
+    "textId" : "noAlarm",
+    "text" : "No Alarm"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Cancel"
+  },
+  {
+    "textId" : "saveAlarmBox",
+    "text" : "Save"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Cancell"
   },
   {
     "textId" : "vibrateText",
@@ -72,19 +104,163 @@ var language = [
     "text" : "Showing stats for"
   },
   {
+    "textId" : "pomodoroTotalTime",
+    "text" : "Pomodoros Total Time:"
+  },
+  {
+    "textId" : "pomodorosFinished",
+    "text" : "Pomodoros Finished:"
+  },
+  {
+    "textId" : "successRate",
+    "text" : "Success Rate:"
+  },
+  {
+    "textId" : "seeChartBtn",
+    "text" : "See Charts"
+  },
+  {
     "textId" : "minorText",
     "text" : "No active tasks"
   },
   {
-    "textId" : "color1",
+    "textId" : "selectedTheme",
     "text" : "Green"
   },
   {
-    "textId" : "color2",
+    "textId" : "greenColor",
+    "text" : "Green"
+  },
+  {
+    "textId" : "blueColor",
     "text" : "Blue"
   },
   {
-    "textId" : "color3",
+    "textId" : "pinkColor",
     "text" : "Pink"
   },
+  {
+    "textId" : "classicColor",
+    "text" : "Classic"
+  },
+  {
+    "textId" : "brightColor",
+    "text" : "Bright"
+  },
+  {
+    "textId" : "cancelThemeBox",
+    "text" : "Cancel"
+  },
+  {
+    "textId" : "saveThemeBox",
+    "text" : "Save"
+  },
+  {
+    "textId" : "confirmDeleteText",
+    "text" : "Are you sure you want to delete this task?"
+  },
+  {
+    "textId" : "deleteNo",
+    "text" : "No"
+  },
+  {
+    "textId" : "deleteYes",
+    "text" : "Yes"
+  },
+  {
+    "textId" : "errorText",
+    "text" : "Error"
+  },
+  {
+    "textId" : "errorMsg",
+    "text" : "Please fill all fields before adding tasks"
+  },
+  {
+    "textId" : "congratulationsText",
+    "text" : "Congratulations!"
+  },
+  {
+    "textId" : "congratsText",
+    "text" : "Awesome! You finished a Pomodoro!"
+  },
+  {
+    "textId" : "startBreakText",
+    "text" : "Start short Break"
+  },
+  {
+    "textId" : "startLongBreakText",
+    "text" : "Start Long Break"
+  },
+  {
+    "textId" : "quitText",
+    "text" : "Quit"
+  },
+  {
+    "textId" : "statsTitleText",
+    "text" : "Statistics"
+  },
+  {
+    "textId" : "donutChartBtn",
+    "text" : "Donut Chart"
+  },
+  {
+    "textId" : "barChartBtn",
+    "text" : "Bar Chart"
+  },
+  {
+    "textId" : "chartBackBtn",
+    "text" : "Back"
+  },
+  {
+    "textId" : "editTaskText",
+    "text" : "Edit Task"
+  },
+  {
+    "textId" : "editTaskNameText",
+    "text" : "Task Name"
+  },
+  {
+    "textId" : "editStartTimeText",
+    "text" : "Start Time"
+  },
+  {
+    "textId" : "editPLength",
+    "text" : "Pomodoro Length"
+  },
+  {
+    "textId" : "editSbreakText",
+    "text" : "Short Break Length"
+  },
+  {
+    "textId" : "editLBreakText",
+    "text" : "Long Break Length"
+  },
+  {
+    "textId" : "deleteTaskText",
+    "text" : "Delete Task"
+  },
+  {
+    "textId" : "cancelEditBtn",
+    "text" : "Cancel"
+  },
+  {
+    "textId" : "addChangesBtn",
+    "text" : "Save"
+  },
+  {
+    "textId" : "startTaskText",
+    "text" : "Start Task"
+  },
+  {
+    "textId" : "startWorkText",
+    "text" : "Do you want to start working on this task now?"
+  },
+  {
+    "textId" : "startNo",
+    "text" : "No"
+  },
+  {
+    "textId" : "startYes",
+    "text" : "Yes"
+  }
 ];

fc92027714c6c58e2d18d347a9c2c740311dce8d
Sarahjdes <sjdesrochers@challengeu.com>
2017-02-14 18:46:10 -0500

    Create fr.js
    Let me know if anything is missing! Checkout  "textId" : "selectedTheme", should it really be a color? If "selected theme" is the right text in EN, then the right FR text is "Thème sélectionné"

---
+++ localization/fr.js
@@ -0,0 +1,270 @@
+var language = [
+  {
+    "textId" : "tasksBtn",
+    "text" : "Tâches"
+  },
+  {
+    "textId" : "statsBtn",
+    "text" : "Statistiques"
+  },
+  {
+    "textId" : "historyBtn",
+    "text" : "Historique"
+  },
+  {
+    "textId" : "settingsTitleText",
+    "text" : "Paramètres"
+  },
+  {
+    "textId" : "changeStyle",
+    "text" : "Changer le style"
+  },
+  {
+    "textId" : "alarmSound",
+    "text" : "Son de l'alarme"
+  },
+  {
+    "textId" : "alarmVolume",
+    "text" : "Volume de l'alarme"
+  },
+  {
+    "textId" : "noAlarm",
+    "text" : "Pas d'alarme"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Annuler"
+  },
+  {
+    "textId" : "saveAlarmBox",
+    "text" : "Sauvegarder"
+  },
+  {
+    "textId" : "cancelAlarmBox",
+    "text" : "Annuler"
+  },
+  {
+    "textId" : "vibrateText",
+    "text" : "Vibrer lorsque l'alarme sonne"
+  },
+  {
+    "textId" : "sendFeedBack",
+    "text" : "Envoyer un commentaire"
+  },
+  {
+    "textId" : "rateApp",
+    "text" : "Noter cette application"
+  },
+  {
+    "textId" : "addTaskTitle",
+    "text" : "Ajouter une tâche"
+  },
+  {
+    "textId" : "taskNameText",
+    "text" : "Nom de la tâche"
+  },
+  {
+    "textId" : "studyTag",
+    "text" : "Étude"
+  },
+  {
+    "textId" : "workTag",
+    "text" : "Travail"
+  },
+  {
+    "textId" : "researchTag",
+    "text" : "Recherche"
+  },
+  {
+    "textId" : "sportTag",
+    "text" : "Sport"
+  },
+  {
+    "textId" : "startTimeText",
+    "text" : "Temps de départ"
+  },
+  {
+    "textId" : "lengthText",
+    "text" : "Durée du Pomodoro"
+  },
+  {
+    "textId" : "shortBreakText",
+    "text" : "Durée d'une courte pause"
+  },
+  {
+    "textId" : "longBreakText",
+    "text" : "Durée d'une longue pause"
+  },
+  {
+    "textId" : "addNewTaskBtn",
+    "text" : "Ajouter"
+  },
+  {
+    "textId" : "statsTitle",
+    "text" : "Statistiques pour"
+  },
+  {
+    "textId" : "pomodoroTotalTime",
+    "text" : "Temps total des Pomodoros:"
+  },
+  {
+    "textId" : "pomodorosFinished",
+    "text" : "Pomodoros terminés:"
+  },
+  {
+    "textId" : "pomodorosFailed",
+    "text" : "Pomodoros non terminés:"
+  },
+  {
+    "textId" : "successRate",
+    "text" : "Succès:"
+  },
+  {
+    "textId" : "seeChartBtn",
+    "text" : "Voir les graphiques"
+  },
+  {
+    "textId" : "minorText",
+    "text" : "Aucune tâche active"
+  },
+  {
+    "textId" : "selectedTheme",
+    "text" : "Vert"
+  },
+  {
+    "textId" : "greenColor",
+    "text" : "Vert"
+  },
+  {
+    "textId" : "blueColor",
+    "text" : "Bleu"
+  },
+  {
+    "textId" : "pinkColor",
+    "text" : "Rose"
+  },
+  {
+    "textId" : "classicColor",
+    "text" : "Classique"
+  },
+  {
+    "textId" : "brightColor",
+    "text" : "Couleurs vives"
+  },
+  {
+    "textId" : "cancelThemeBox",
+    "text" : "Annuler"
+  },
+  {
+    "textId" : "saveThemeBox",
+    "text" : "Sauvegarder"
+  },
+  {
+    "textId" : "confirmDeleteText",
+    "text" : "Êtes-vous certain de vouloir supprimer cette tâche?"
+  },
+  {
+    "textId" : "deleteNo",
+    "text" : "Non"
+  },
+  {
+    "textId" : "deleteYes",
+    "text" : "Oui"
+  },
+  {
+    "textId" : "errorText",
+    "text" : "Erreur"
+  },
+  {
+    "textId" : "errorMsg",
+    "text" : "Veuiller remplir tous les champs avant d'ajouter des tâches"
+  },
+  {
+    "textId" : "congratulationsText",
+    "text" : "Félicitations!"
+  },
+  {
+    "textId" : "congratsText",
+    "text" : "Super! Vous avez terminé un Pomodoro!"
+  },
+  {
+    "textId" : "startBreakText",
+    "text" : "Prenez une courte pause"
+  },
+  {
+    "textId" : "startLongBreakText",
+    "text" : "Prenez une longue pausek"
+  },
+  {
+    "textId" : "quitText",
+    "text" : "Quitter"
+  },
+  {
+    "textId" : "statsTitleText",
+    "text" : "Statistiques"
+  },
+  {
+    "textId" : "donutChartBtn",
+    "text" : "Diagramme circulaire"
+  },
+  {
+    "textId" : "barChartBtn",
+    "text" : "Diagramme à bandes"
+  },
+  {
+    "textId" : "chartBackBtn",
+    "text" : "Retour"
+  },
+  {
+    "textId" : "editTaskText",
+    "text" : "Modifier la tâche"
+  },
+  {
+    "textId" : "editTaskNameText",
+    "text" : "Nom de la tâche"
+  },
+  {
+    "textId" : "editStartTimeText",
+    "text" : "Temps de départ"
+  },
+  {
+    "textId" : "editPLength",
+    "text" : "Durée du Pomodoro"
+  },
+  {
+    "textId" : "editSbreakText",
+    "text" : "Durée d'une courte pause"
+  },
+  {
+    "textId" : "editLBreakText",
+    "text" : "Durée d'une longue pause"
+  },
+  {
+    "textId" : "deleteTaskText",
+    "text" : "Supprimer la tâche"
+  },
+  {
+    "textId" : "cancelEditBtn",
+    "text" : "Annuler"
+  },
+  {
+    "textId" : "addChangesBtn",
+    "text" : "Sauvegarder"
+  },
+  {
+    "textId" : "startTaskText",
+    "text" : "Commencer la tâche"
+  },
+  {
+    "textId" : "startWorkText",
+    "text" : "Voulez-vous commencer à travailler sur cette tâche maintenant?"
+  },
+  {
+    "textId" : "startNo",
+    "text" : "Non"
+  },
+  {
+    "textId" : "startYes",
+    "text" : "Oui"
+  }
+];
